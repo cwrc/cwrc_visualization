94e2910fd86ae4f5dce9035cd00998579aedf7b4
Luke Bainbridge <luke@echidna.ca>
2016-04-01 14:59:22 -0400

    Initial commit

---
+++ templates/cwrc-visualization.tpl.php
@@ -0,0 +1,6 @@
+<div class="iframe-wrapper">
+  <iframe class="<?php print $classes ?>" src="<?php print $url; ?>"></iframe>
+</div>
+<div class="fullscreen-link-wrapper">
+  <a href="<?php print $url; ?>" target="_blank"><?php print t('Open fullscreen'); ?></a>
+</div>
