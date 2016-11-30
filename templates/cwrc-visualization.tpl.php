c51ec04652d91d749c74c56976a83739c07d8f51
M Parker <mparker17@536298.no-reply.drupal.org>
2016-11-30 13:27:19 -0500

    Bring module to coding standards.

--- templates/cwrc-visualization.tpl.php
+++ templates/cwrc-visualization.tpl.php
@@ -1,3 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Displays an iframe for a visualization.
+ *
+ * Available variables:
+ * - $classes: A set of classes to apply to the iframe element.
+ * - $url: The URL to use for the iframe source.
+ */
+?>
 <div class="iframe-wrapper">
   <iframe class="<?php print $classes ?>" src="<?php print $url; ?>"></iframe>
 </div>
